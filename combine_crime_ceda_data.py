e5d02c614bd219e2d3456275f149b5a4258fb996
AG <>
2026-07-15 11:58:39 +0100

    intially subsetted the crime archive by time and owning force

--- combine_crime_ceda_data.py
+++ combine_crime_ceda_data.py
@@ -13,6 +13,7 @@ Mapping from lat lon to the 1km grid
 
 ###########################################################
 # %% Import modules
+from pandas.core.frame import DataFrame
 from _duckdb import DuckDBPyConnection
 import os
 import duckdb
@@ -33,6 +34,9 @@ cwd: str = os.getcwd()
 crime_data_dir: Path = Path(cwd) / 'data' / 'police_archives'
 crime_db: Path = crime_data_dir/'crime_archive.db'
 
+# %% User Inputs
+make_table:bool = False
+
 
 
 # %%
@@ -42,7 +46,65 @@ con: DuckDBPyConnection = duckdb.connect(database=crime_db)
 # introspect
 con.execute(query="SHOW TABLES").fetchall()
 con.execute(query="SELECT * FROM street_data LIMIT 5;").df()
+con.execute(query="SELECT COUNT(*) FROM street_data;").df()
+
+# Look at data type for later filtering
+con.execute(query="SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'street_data' AND column_name = 'Month';").fetchall()
+# Month column is VARCHAR
+
+# have a look at all months
+con.execute(query="""SELECT DISTINCT 
+                            Month 
+                    FROM street_data 
+                    WHERE Month BETWEEN '2016-01' AND '2025-12'
+                    ORDER BY Month;
+                    """).df()
+# 120 rows found so it is getting the time filtering ok 
+
+
+# look at owning forces for later filtering
+force_list: DataFrame = con.execute(query="""SELECT DISTINCT "Falls Within" FROM street_data;""").df()
+# Norfolk: Norfolk Constabulary
+# Suffolk: Suffolk Constabulary
+
+
+
+
+
+## Interesting question: Initially looking at Norfolk and Suffolk
+# for initial scoping of project. This is rurual, rural/urban (not large city urban, metropolitian)
+# intersting to compare the results to UK wide vs this geographic subset 
+# to see if differences emerge
 
 # %%
-#TODO: just want data spanning 2016-01 to 2025-12
-# TODO: drop unnecessary columns
+
+
+# select subsection of crime data needed for project
+# Crime ID, Month, Longitude, Latitude, Crime type
+# filter to just Norfolk and Suffolk between 2016 and 2026
+#write this to a new table
+
+if make_table:
+        subset_query = """
+                CREATE OR REPLACE TABLE crimetology_NS AS
+                SELECT "Crime ID", 
+                        Month, 
+                        Longitude, 
+                        Latitude, 
+                        "Crime type"
+                FROM street_data 
+                WHERE "Falls within" IN ('Norfolk Constabulary', 'Suffolk Constabulary')
+                AND Month BETWEEN '2016-01' AND '2025-12';
+                """
+        con.execute(query=subset_query)
+
+
+#table size = 1314474 rows
+# original street data archive = 92362169
+
+
+#now open and have a look
+con.execute(query="SHOW TABLES").fetchall()
+con.execute(query="SELECT * FROM crimetology_NS LIMIT 15;").df()
+
+## map to weather
